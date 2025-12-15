de48a09e70cac3c4ae9347636988c3aed425f018
Satyam Verma <satyam.verma.og27@gmail.com>
2025-12-15 22:21:38 +0530

    everything ok !! folder structure cteated,expres server made, connected server with mongo db

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,16 @@
 const express = require('express')
+
+const cors = require('cors')
 require("./models/db")
 require('dotenv').config()
 const app = express()
 const port = process.env.port || 3000;
 
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(cors())
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
