a77d60c12112a5a4ff5501e4a57c7b7c414f5b9b
Satyam Verma <satyam.verma.og27@gmail.com>
2025-12-22 23:16:03 +0530

    basic routing of authentication is done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
-
+import React from 'react'
+import {Routes , Route, Navigate} from 'react-router-dom'
+import Login from './pages/login.jsx'
+import Signup from './pages/signup.jsx'
+import Home from './pages/home.jsx'
 
 function App() {
   
 
   return (
-    <>
-    
-    </>
+    <div className='App'>
+      <Routes>
+        <Route path='/' element={<Login/>} />
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/home' element={<Home/>}/>
+      </Routes>
+    </div>
   )
 }
 
---
+++ frontend/src/pages/login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+      <h1> login route</h1>
+    </div>
+  )
+}
+
+export default Login
